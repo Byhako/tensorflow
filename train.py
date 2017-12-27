6eba4cf8536602aafdc1717b894facf40387dfe4
Ruben E Acosta <eldragondera1@gmail.com>
2017-12-26 23:43:14 -0500

    train basic end

--- train.py
+++ train.py
@@ -1,26 +1,35 @@
 import tensorflow as tf
 
-sess = tf.Session()
-
+# Model parameters
 w = tf.Variable([0.3], dtype = tf.float32)
 b = tf.Variable([-0.3], dtype = tf.float32)
+
+# Model input and output
 x = tf.placeholder(tf.float32)
-
 lineal = w*x + b
-
-init = tf.global_variables_initializer()
-sess.run(init)
-
 y = tf.placeholder(tf.float32)
+
+# loss
 squares = tf.square(lineal - y)
 loss = tf.reduce_sum(squares)
 
-
+# optimizer
 optimizer = tf.train.GradientDescentOptimizer(0.01)
 train = optimizer.minimize(loss)
 
+# training data
+x_train = [1, 2, 3, 4]
+y_train = [0, -1, -2, -3]
+
+# taining loop
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+
 for i in range(1000):
-	sess.run(train, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]})
+	sess.run(train, {x: x_train, y: y_train})
 
-print(sess.run([w, b]))
+# evaluate training accuracy
+curr_w, curr_b, curr_loss = sess.run([w, b, loss], {x: x_train, y: y_train})
 
+print('\nw: %s, b: %s, loss: %s'%(curr_w, curr_b, curr_loss))
